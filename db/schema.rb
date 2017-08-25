c5ec626beb60f011ca6ffab0b5c76ea183911ff0
Jordan Rader <jdavisrader@gmail.com>
2017-08-25 15:31:19 -0600

    added seeds file for dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825205009) do
+ActiveRecord::Schema.define(version: 20170825212757) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170825205009) do
   create_table "portfolios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.string "subtitle"
-    t.string "body"
+    t.text "body"
     t.text "main_image"
     t.text "thumb_image"
     t.datetime "created_at", null: false
